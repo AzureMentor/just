4ee1bdcc50d197fe330f54bc48c6674b92796925
Elizabeth Craig <elcraig@microsoft.com>
2019-04-04 15:28:21 -0700

    Assorted updates

--- packages/just-stack-single-lib/template/jest.config.js
+++ packages/just-stack-single-lib/template/jest.config.js
@@ -1,5 +1,8 @@
+const path = require('path');
+
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  rootDir: 'src'
+  rootDir: 'src',
+  reporters: [path.resolve(__dirname, 'node_modules/just-scripts/lib/jest/JestReporter.js')]
 };
